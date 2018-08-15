49f235e32b372dbc046b4c0c07eb5f7d1a7fef70
Robin Tram <robin.tram@gmail.com>
2018-08-15 23:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} day until ${holiday}!`
+}
